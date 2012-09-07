9565bf07e6de2ada347b347fa2908c1b53d2aded
Chas Emerick <chas@cemerick.com>
2012-09-07 18:57:41 -0400

    bump version, dependency versions, and tweak ignores

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject valip "0.2.0"
-  :description "Functional validation library"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+(defproject com.cemerick/valip "0.3.0"
+  :description "Functional validation library, forked from https://github.com/weavejester/valip"
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [commons-validator "1.3.1"]
-                 [clj-time "0.3.0"]])
+                 [clj-time/clj-time "0.4.4"]])
