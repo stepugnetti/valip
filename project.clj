e23a01a41543840660c87357e4154963f0df7e06
Chas Emerick <chas@cemerick.com>
2012-09-08 01:42:35 -0400

    0.3.1

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject com.cemerick/valip "0.3.0"
-  :description "Functional validation library, forked from https://github.com/weavejester/valip"
+(defproject com.cemerick/valip "0.3.1"
+  :description "Functional validation library for Clojure and ClojureScript, forked from https://github.com/weavejester/valip"
+  :url "http://github.com/cemerick/valip"
   :dependencies [[org.clojure/clojure "1.4.0"]])
