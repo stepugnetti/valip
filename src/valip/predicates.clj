be050a7aafa7dbb7f875744420da3572dcca6c05
Chas Emerick <chas@cemerick.com>
2012-09-08 00:32:27 -0400

    Significant changes: * Most predicates made portable between ClojureScript/Clojure * moved platform-specific preds to valip.{js|java}.predicates * Eliminate java.text-based numerics handling (locale- and JVM-dependent) * Eliminate date validations * Assertions eliminated (shouldn't be used in code necessary to ensure "correct operation": http://docs.oracle.com/javase/6/docs/technotes/guides/language/assert.html) * pfn and defpredicate macros added to allow for definition of preds that have dependents

--- src/valip/predicates.clj
+++ src/valip/predicates.clj
@@ -1,20 +1,13 @@
 (ns valip.predicates
-  "Predicates useful for validating input strings, such as ones from a HTML
-  form."
-  (:require [clojure.string :as string]
-            [clj-time.format :as time-format])
-  (:import
-    [java.net URL MalformedURLException]
-    java.util.Hashtable
-    javax.naming.NamingException
-    javax.naming.directory.InitialDirContext
-    [org.apache.commons.validator.routines IntegerValidator
-                                           DoubleValidator]))
+  "Predicates useful for validating input strings, such as ones from HTML forms.
+All predicates in this namespace are considered portable between different
+Clojure implementations."
+  (:require [clojure.string :as str]))
 
 (defn present?
   "Returns false if x is nil or blank, true otherwise."
   [x]
-  (not (string/blank? x)))
+  (not (str/blank? x)))
 
 (defn matches
   "Creates a predicate that returns true if the supplied regular expression
@@ -40,122 +33,73 @@
   invalid, as this syntax is not commonly supported in practise. The domain of
   the email address is not checked for validity."
   [email]
-  {:pre [(present? email)]}
   (let [re (str "(?i)[a-z0-9!#$%&'*+/=?^_`{|}~-]+"
                 "(?:\\.[a-z0-9!#$%&'*+/=?" "^_`{|}~-]+)*"
                 "@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+"
                 "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]
     (boolean (re-matches (re-pattern re) email))))
 
-(defn- dns-lookup [^String hostname ^String type]
-  (let [params {"java.naming.factory.initial"
-                "com.sun.jndi.dns.DnsContextFactory"}]
-    (try
-      (.. (InitialDirContext. (Hashtable. params))
-          (getAttributes hostname (into-array [type]))
-          (get type))
-      (catch NamingException _
-        nil))))
-
-(defn valid-email-domain?
-  "Returns true if the domain of the supplied email address has a MX DNS entry."
-  [email]
-  {:pre [(email-address? email)]}
-  (if-let [domain (second (re-matches #".*@(.*)" email))]
-    (boolean (dns-lookup domain "MX"))))
-
-(defn url?
-  "Returns true if the string is a valid URL."
+(defn integer-string?
+  "Returns true if the string represents an integer."
   [s]
-  {:pre [(present? s)]}
-  (try
-    (URL. s) true
-    (catch MalformedURLException _ false)))
+  (boolean (re-matches #"\s*[+-]?\d+\s*" s)))
+
+(defn decimal-string?
+  "Returns true if the string represents a decimal number."
+  [s]
+  (boolean (re-matches #"\s*[+-]?\d+(\.\d+(M|M|N)?)?\s*" s)))
 
 (defn digits?
   "Returns true if a string consists only of numerical digits."
   [s]
-  {:pre [(present? s)]}
   (boolean (re-matches #"\d+" s)))
 
 (defn alphanumeric?
   "Returns true if a string consists only of alphanumeric characters."
   [s]
-  {:pre [(present? s)]}
-  (boolean (re-matches #"[A-Za-z0-9]+")))
-
-(defn integer-string?
-  "Returns true if the string represents an integer."
-  [s]
-  {:pre [(present? s)]}
-  (boolean (.validate (IntegerValidator.) s)))
-
-(defn decimal-string?
-  "Returns true if the string represents a decimal number."
-  [s]
-  {:pre [(present? s)]}
-  (boolean (.validate (DoubleValidator.) s)))
+  (boolean (re-matches #"[A-Za-z0-9]+" s)))
 
 (defn- parse-number [x]
-  {:pre [(present? x)]}
-  (if (string? x)
-    (.validate (DoubleValidator.) x)
-    x))
+  (if (and (string? x) (re-matches #"\s*[+-]?\d+(\.\d+M|M|N)?\s*" x))
+    (read-string x)))
 
 (defn gt
   "Creates a predicate function for checking if a value is numerically greater
   than the specified number."
   [n]
-  (fn [x] (> (parse-number x) n)))
+  (fn [x] (if-let [x (parse-number x)] (> x n))))
 
 (defn lt
   "Creates a predicate function for checking if a value is numerically less
   than the specified number."
   [n]
-  (fn [x] (< (parse-number x) n)))
+  (fn [x] (if-let [x (parse-number x)] (< x n))))
 
 (defn gte
   "Creates a predicate function for checking if a value is numerically greater
   than or equal to the specified number."
   [n]
-  (fn [x] (>= (parse-number x) n)))
+  (fn [x] (if-let [x (parse-number x)] (>= x n))))
 
 (defn lte
   "Creates a predicate function for checking if a value is numerically less
   than or equal to the specified number."
   [n]
-  (fn [x] (<= (parse-number x) n)))
+  (fn [x] (if-let [x (parse-number x)] (<= x n))))
 
-(def ^{:doc "Alias for gt"} over gt)
+(def over "Alias for gt" gt)
 
-(def ^{:doc "Alias for lt"} under lt)
+(def under "Alias for lt" lt)
 
-(def ^{:doc "Alias for gte"} at-least gte)
+(def at-least "Alias for gte" gte)
 
-(def ^{:doc "Alias for lte"} at-most lte)
+(def at-most "Alias for lte" lte)
 
 (defn between
   "Creates a predicate function for checking whether a number is between two
   values (inclusive)."
   [min max]
   (fn [x]
-    (let [x (parse-number x)]
+    (if-let [x (parse-number x)]
       (and (>= x min) (<= x max)))))
 
-(defn- parse-date-time [format input]
-  {:pre [(present? input)]}
-  (let [formatter (time-format/formatter format)]
-    (try
-      (time-format/parse formatter input)
-      (catch IllegalArgumentException _ nil))))
-
-(defn date-format
-  "Creates a function for parsing a date using the supplied format string."
-  [format]
-  (partial parse-date-time format))
-
-(defn html5-date?
-  "Returns true if the string is one that could be returned by a HTML5 date
-  input element."
-  [s]
-  (boolean (parse-date-time "yyyy-MM-dd" s)))
